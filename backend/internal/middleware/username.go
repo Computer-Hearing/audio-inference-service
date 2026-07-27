b656b0b0e2b381ff871d3ca672dde1fbb24d82ff
Vlad <golikv070@gmail.com>
2026-07-27 23:28:32 +0300

    middleware

---
+++ backend/internal/middleware/username.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"audio-inference-service/pkg"
+	"context"
+	"log/slog"
+	"net/http"
+)
+
+const UserContextKey = "username"
+
+func CheckUsernameCookie(logger *slog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			cookie, err := r.Cookie(pkg.UsernameCookieKey)
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				_, _ = w.Write([]byte(`user is unauthorized`))
+				logger.Warn("user is unauthorized", "ip", r.RemoteAddr, "userAgent", r.UserAgent())
+				return
+			}
+
+			userGSSC := cookie.Value
+			//TODO сделать GSSC
+			username := userGSSC
+			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserContextKey, username)))
+		})
+	}
+}
